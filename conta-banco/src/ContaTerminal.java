5c9631669c99fd30bc8cfed1049682b8c1d962ab
marcus <marcus.porfirio009@gmail.com>
2024-05-23 21:23:39 -0300

    Implementação das informações do projeto conta bancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    //int numero;
+    //String agencia;
+    //String nomeCliente;
+    //double saldo;
+      
+    //TODO:CONHECER E IMPORTAR A CLASSE SCANNER
+    
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite sua conta: ");
+    int numero = scanner.nextInt();
+
+    System.out.println("Digite sua Agencia: ");
+    String agencia = scanner.next();
+    
+    System.out.println("Digite seu nome: ");
+    String nomeCliente = scanner.next();
+    
+    System.out.println("Digite o saldo: ");
+    double saldo = scanner.nextDouble();
+
+      //EXIBIR AS MESAGENS PARA O NOSSO USUARIO
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar a conta em nosso banco, sua agência é: " + agencia + ", conta: " + numero + " e seu saldo é de: " + saldo +" que já está disponível para saque");
+      //OBTER PELO SCANNER OS VALORES DIGITADOS NO TERMINAL
+
+      //EXIBIR A MENSAGEM CONTA CRIADA
     }
 }
